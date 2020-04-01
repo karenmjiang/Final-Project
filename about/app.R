653390e848894d370b9ed534895bfe9d10958c74
Karen <35306227+karenmjiang@users.noreply.github.com>
2020-03-31 21:25:22 -0700

    shiny stuff

--- about/app.R
+++ about/app.R
@@ -1,65 +1,144 @@
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(ggplot2)
+library(readr)
+library(tidyverse)
+
+aco <-
+    read.csv(
+        "data/2018_Shared_Savings_Program__SSP__Accountable_Care_Organizations__ACO__PUF.csv"
+    ) %>%
+    select(
+        ACO_ID,
+        Initial_Start_Date,
+        Agree_Type,
+        ACO_State,
+        QualScore,
+        Current_Track_1,
+        Current_Track_1_Plus,
+        Current_Track_2,
+        Current_Track_3,
+        Initial_Track_1,
+        Initial_Track_1_Plus,
+        Initial_Track_2,
+        Initial_Track_3
+    ) %>%
+    mutate(ACO_State = map(ACO_State,
+                           ~ unlist(str_split(.x, ", ")))) %>%
+    mutate(num_states = map(ACO_State, length),
+           num_states = as.integer(num_states)) %>%
+    gather(current, flag, Current_Track_1:Current_Track_3) %>%
+    filter(flag == 1) %>%
+    select(-flag) %>%
+    gather(initial, flag2, Initial_Track_1:Initial_Track_3) %>%
+    filter(flag2 == 1) %>%
+    select(-flag2) %>%
+    mutate(
+        Initial_Start_Date = as.Date(Initial_Start_Date, format = "%m/%d/%Y"),
+        Initial_Start_Year = as.factor(format(Initial_Start_Date, "%Y"))
+    ) 
 
 # Define UI for application that draws a histogram
 ui <- navbarPage(
     "Value Based Purchasing",
-    tabPanel("Model",
+    tabPanel("Data Exploration",
              fluidPage(
-                 titlePanel("Model Title"),
+                 titlePanel("Accountable Care Organizations"),
                  sidebarLayout(
                      sidebarPanel(
-                         selectInput(
-                             "plot_type",
-                             "Plot Type",
-                             c("Option A" = "a", "Option B" = "b")
-                         )),
-                     mainPanel(plotOutput("line_plot")))
+                         radioButtons(inputId = "agree_type",
+                                     label = "Agreement Type",
+                                     choices = unique(aco$Agree_Type),
+                                     selected = "Renewal"
+                            ),
+                         
+                         selectInput(inputId = "initial_track", 
+                                     label = "Initial Track", 
+                                     choices = aco$initial,
+                                     multiple = TRUE),
+                         
+                         selectInput(inputId = "current_track", 
+                                     label = "Current Track", 
+                                     choices = aco$current,
+                                     multiple = TRUE,)
+                     ),
+                     mainPanel(
+                         plotOutput("graph")
+                     )
+                 )
              )),
-    tabPanel("Discussion",
-             titlePanel("Discussion Title"),
-             p("Tour of the modeling choices you made and 
-              an explanation of why you made them")),
-    tabPanel("About", 
-             titlePanel("About"),
-             p("This project looks to examine the ACO landscape in 2018. Can we identify predictors or find trends in successful ACOs?"),
-             h3("Project Background"),
-             p("In 2018, the National Health Expenditure accounted for 17.7% of GDP, which equates $3.6 trillion, or $11,172.00 per person. In order to control healthcare costs, the Center for Medicare and Medicaid Services (CMS) is shifting towards value-based payment systems through the Medicare Shared Saving Program (MSSP). Accountable Care Organizations (ACOs) represent one voluntary and emergent payment structure under MSSP."),
-             p("Medicare traditionally paid providers and hospitals through a Fee For Service (FFS) payment system. That is, payments are made to providers and hospitals for each service provided. This creates the incentive to provide as many services as possible for patients. This has led to overutilization and increased costs for the health system and duplicative services and poor coordination of care for patients."),
-             p("ACOs attempt to remedy some of these issues.
-An ACO is a group of hospitals, physicians, other providers who come together voluntarily to provide better coordinated care for their Medicare Patients and limit unncessary spending. ACOs engage in financial risk, by accepting a capitated payment per patient per month. If the spending for care is less than the alloted amount, the ACO is able to keep the difference. However, if spending exceeds the amount, the ACO must pay back the difference to CMS. "),
-             p("In order to prevent skimping on care for patients, ACOs must also provide outcome data in order to be eligible for savings."),
-             h3("Data Sources"),
-             p(a(href = "https://data.cms.gov/Special-Programs-Initiatives-Medicare-Shared-Savin/2018-Number-of-ACO-Assigned-Beneficiaries-by-Count/qu23-5j39
-", "2018 Number of ACO Assigned Beneficiares")),
-             p("Assigned beneficiary person-year counts are based on certified ACO Participant Lists for the performance year (PY) 2018 and based on the assignment methodology and definition of dual eligible beneficiaries in effect for that performance year."),
-             p(a(href = "https://data.cms.gov/Special-Programs-Initiatives-Medicare-Shared-Savin/2018-County-level-FFS-Data-for-Shared-Savings-Prog/sju4-8k6c","2018 County Level FFS Data")),
-             p("Aggregate data consisting of per capita Parts A and B FFS expenditures, average CMS-HCC prospective risk scores and total person-years for assignable beneficiaries by Medicare enrollment type (End Stage Renal Disease (ESRD), disabled, aged/dual eligible, aged/non-dual eligible). It is the intent of CM to publish this PUF annually, in the summer following the conclusion of the calendar year to which it relates."),
-             p(a(href = "https://data.cms.gov/Special-Programs-Initiatives-Medicare-Shared-Savin/2018-Shared-Savings-Program-SSP-Accountable-Care-O/v47u-yq84","2018 Shared Savings Program Accountable Care Organization")),
-             p("2018 SSP ACO PUF - To address the increasing number of requests for SSP ACO data, the Centers for Medicare (CM) has created a standard analytical file that CMS can use to efficiently satisfy these requests. It is the intent of CM to publish the ACO-level public-use file (PUF) that contains ACO-specific metrics as well as summarized beneficiary and provider information for each performance year of the SSP program."),
-             h3("About Me"),
-             p("My name is Karen Jiang and I am an MPH Candidate in Health Policy at Harvard T. H. Chan School of Public Health. 
-             You can reach me at karenjiang@hsph.harvard.edu")))
+    
+    tabPanel(
+        "Discussion",
+        titlePanel("Discussion Title"),
+        p(
+            "Tour of the modeling choices you made and
+              an explanation of why you made them"
+        )
+    ),
+    
+    tabPanel(
+        "About",
+        titlePanel("About"),
+        p(
+            "This project looks to examine the ACO landscape in 2018. Can we identify predictors or find trends in successful ACOs?"
+        ),
+        h3("Project Background"),
+        p(
+            "In 2018, the National Health Expenditure accounted for 17.7% of GDP, which equates $3.6 trillion, or $11,172.00 per person. In order to control healthcare costs, the Center for Medicare and Medicaid Services (CMS) is shifting towards value-based payment systems through the Medicare Shared Saving Program (MSSP). Accountable Care Organizations (ACOs) represent one voluntary and emergent payment structure under MSSP."
+        ),
+        p(
+            "Medicare traditionally paid providers and hospitals through a Fee For Service (FFS) payment system. That is, payments are made to providers and hospitals for each service provided. This creates the incentive to provide as many services as possible for patients. This has led to overutilization and increased costs for the health system and duplicative services and poor coordination of care for patients."
+        ),
+        p(
+            "ACOs attempt to remedy some of these issues.
+An ACO is a group of hospitals, physicians, other providers who come together voluntarily to provide better coordinated care for their Medicare Patients and limit unncessary spending. ACOs engage in financial risk, by accepting a capitated payment per patient per month. If the spending for care is less than the alloted amount, the ACO is able to keep the difference. However, if spending exceeds the amount, the ACO must pay back the difference to CMS. "
+        ),
+        p(
+            "In order to prevent skimping on care for patients, ACOs must also provide outcome data in order to be eligible for savings."
+        ),
+        h3("Data Sources"),
+        p(
+            a(href = "https://data.cms.gov/Special-Programs-Initiatives-Medicare-Shared-Savin/2018-Number-of-ACO-Assigned-Beneficiaries-by-Count/qu23-5j39
+", "2018 Number of ACO Assigned Beneficiares")
+        ),
+        p(
+            "Assigned beneficiary person-year counts are based on certified ACO Participant Lists for the performance year (PY) 2018 and based on the assignment methodology and definition of dual eligible beneficiaries in effect for that performance year."
+        ),
+        p(
+            a(href = "https://data.cms.gov/Special-Programs-Initiatives-Medicare-Shared-Savin/2018-County-level-FFS-Data-for-Shared-Savings-Prog/sju4-8k6c", "2018 County Level FFS Data")
+        ),
+        p(
+            "Aggregate data consisting of per capita Parts A and B FFS expenditures, average CMS-HCC prospective risk scores and total person-years for assignable beneficiaries by Medicare enrollment type (End Stage Renal Disease (ESRD), disabled, aged/dual eligible, aged/non-dual eligible). It is the intent of CM to publish this PUF annually, in the summer following the conclusion of the calendar year to which it relates."
+        ),
+        p(
+            a(
+                href = "https://data.cms.gov/Special-Programs-Initiatives-Medicare-Shared-Savin/2018-Shared-Savings-Program-SSP-Accountable-Care-O/v47u-yq84",
+                "2018 Shared Savings Program Accountable Care Organization"
+            )
+        ),
+        p(
+            "2018 SSP ACO PUF - To address the increasing number of requests for SSP ACO data, the Centers for Medicare (CM) has created a standard analytical file that CMS can use to efficiently satisfy these requests. It is the intent of CM to publish the ACO-level public-use file (PUF) that contains ACO-specific metrics as well as summarized beneficiary and provider information for each performance year of the SSP program."
+        ),
+        h3("About Me"),
+        p(
+            "My name is Karen Jiang. I am an MPH Candidate in Health Policy at Harvard T. H. Chan School of Public Health.
+             You can reach me at karenjiang@hsph.harvard.edu"
+        )
+    )
+)
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-    
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-        
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    output$graph <- renderPlot({
+        aco %>% 
+            filter(Agree_Type == input$agree_type) %>%
+        ggplot(aes(x = QualScore, fill = current)) + 
+            geom_density(alpha = 0.4) +
+            labs(x = "Quality Score", 
+                 y = "Density") +
+            theme_classic()
     })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
